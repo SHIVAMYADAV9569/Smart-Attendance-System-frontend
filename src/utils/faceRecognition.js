7d28f71ae84a28314c3d3113cf4d65893e949e21
SHIVAMYADAV9569 <shivamyadav12042006@gmail.com>
2026-03-14 10:54:51 +0530

    face recognize api fix

--- src/utils/faceRecognition.js
+++ src/utils/faceRecognition.js
@@ -8,7 +8,7 @@ import * as faceapi from 'face-api.js';
 // Use /models (proxied to backend in dev) or full URL when API_URL is set
 const MODEL_URL = import.meta.env.DEV
   ? '/models'
-  : (import.meta.env.VITE_API_URL || 'http://localhost:3001/api').replace(/\/api\/?$/, '') + '/models';
+  : (import.meta.env.VITE_API_URL/api || 'http://localhost:3001/api').replace(/\/api\/?$/, '') + '/models';
 
 let modelsLoaded = false;
 
